afef9711c5cbce15e28f61fef5dd327b6329b8f2
Bruno S. Duarte <brunosduarte@hotmail.com>
2025-09-02 00:13:18 -0300

    chore: change favicon and fixed name

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-between p-5">
       <Link href="/">
-        <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+        <Image src="/logo.svg" alt="BsDWEAR" width={100} height={26.14} style={{ width: "auto" }} />
       </Link>
 
       <div className="flex items-center gap-3">
